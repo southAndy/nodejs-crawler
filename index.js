934ca5ce3d19b6395897b5a230d5a728e1e8e608
southAndy <zatchbell.1206@gmail.com>
2022-12-19 21:44:24 +0800

    test:getting single website

--- index.js
+++ index.js
@@ -3,27 +3,52 @@ import express from "express"
 import axios from "axios"
 
 let port = 8700;
-// let targetUrl = 'https://www.tfam.museum/Exhibition/Exhibition_Special.aspx?id=714&ddlLang=zh-tw'
-let targetUrl = 'https://www.tnam.museum/exhibition/detail/353'
+let baseUrl = 'https://www.tfam.museum/Exhibition/Exhibition.aspx?ddlLang=zh-tw'
 let app = express();
 
 
 
 app.get('/', function(req, res) {
-  res.send('hello world');
   const fetchData = async ()=>{
+    //存資料的db
+    let exhibitionInfo = {
+      //名稱
+      title:null,
+      showUnit:null,
+      descriptionFilterHtml:null,
+      startDate: "2022/12/15",
+      endDate: null,
+      showInfo:[
+        {
+          //經緯度 for map
+          latitude: "24.157234",
+          longitude: "120.66606",
+          //展覽是否有售票
+          onSale:'Y',
+          //票價
+          price:''
+        }
+      ],
+      imageUrl:[],
+      hitRate:1955,
+    }
+    
     try{
-        let response = await axios.get(targetUrl)
-        let $ = cheerio.load(response.data)
-        //特定DOM's text
-        // console.log($('.info-content').text());
-        console.log($('.info-content').attr());
-        $('.galleryItem').attr()
+          let response = await axios.get(baseUrl)
+          let $ = cheerio.load(response.data)
+          
+          //todo 用find('.text') 產生
+          console.log(123,$('.Exhibition_list').length);
+          // const listArr = $('#ExList').find('.Exhibition_list');
+          // listArr.map((i,el)=>{
+          //   console.log(123,i,el);
+          // })
     }catch(error){
-        console.log('loading');
+        console.log('loading',error.message);
     }
 }
-  fetchData()
+  let x = fetchData()
+  res.send('hello world');
 
   
 });
